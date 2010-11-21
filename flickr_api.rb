6e8e7f23aa9cdc03a5e6b99ec13acef1851ab217
Andrew Evans <andrew@atevans.com>
2010-11-20 20:30:46 -0800

    Gets list of small photos for index

---
+++ flickr_api.rb
@@ -0,0 +1,4 @@
+class FlickrApi
+  include HTTParty
+  base_uri 'http://api.flickr.com/services/rest/'
+end
--- flickrtouch.rb
+++ flickrtouch.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
+require 'yaml'
 require 'haml'
+require 'httparty'
+
+before do
+  @config = YAML.load_file(File.dirname(File.expand_path(__FILE__)) + '/config.yaml')
+end
 
 get '/' do
+  @photos = HTTParty.get('http://api.flickr.com/services/rest/', :query => {:api_key => '184ac67ff5ec7b673f506e74a09b74c0', :method => 'flickr.photos.getRecent', :per_page => '10'})
+  @photos = @photos["rsp"]["photos"]["photo"]
   haml :index
 end
