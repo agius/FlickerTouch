e37ae81a5c329ebd75278238bf53ac0776edc09a
Andrew Evans <andrew@atevans.com>
2010-11-21 17:06:38 -0800

    jQTouch. Basically still working somehow.

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,23 @@
+var jQT = new $.jQTouch({
+    icon: 'jqtouch.png',
+    statusBar: 'black-translucent',
+    preloadImages: [
+        '/themes/jqt/img/chevron_white.png',
+        '/themes/jqt/img/bg_row_select.gif',
+        '/themes/jqt/img/back_button_clicked.png',
+        '/themes/jqt/img/button_clicked.png'
+        ]
+});
 $(window).load(function(){
-  load_more = function(e){
-    e.preventDefault();
-    $.get($('#more').attr('href'), function(data) {
-      $('#photos').append(data);
-    });
-  };
-  $('#more').click(load_more);
+  var gallery_page = $('#more').attr('data-page')
+  $('#more_link').click(function(e){
+    $.get('/?page=' + (gallery_page + 1), function(data) {
+      $('#photos').append(data)
+      gallery_page++
+    })
+  })
+  $('.galleryimg').click(function(e){
+    $('#picture').html('<img src="' + $(e.target).attr('data-photo') + '" />')
+    jQT.goTo('#view')
+  })
 });
